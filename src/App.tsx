1d57f660015bcf6ee2b7b69b512700fb4e85cace
Josh Graham <joshgraham14@gmail.com>
2023-09-22 20:55:59 -0400

    Added selection boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
 import './App.css'
 import { Scale } from './components/Scale'
 import Select from 'react-select'
+import { Option, noteOptions, scaleTypeOptions } from './data'
 
 import { useState } from 'react'
-
-type Option = {
-	value: string
-	label: string
-}
 
 function App() {
 	const [scale, setScale] = useState<string>('')
 	const [note, setNote] = useState<string>('')
 	const [scaleTitle, setScaleTitle] = useState<string>('')
-
-	const noteOptions: Option[] = [
-		{ value: 'C', label: 'C' },
-		{ value: 'Db', label: 'C#/Db' },
-		{ value: 'D', label: 'D' },
-		{ value: 'Eb', label: 'D#/Eb' },
-		{ value: 'E', label: 'E' },
-		{ value: 'F', label: 'F' },
-		{ value: 'Gb', label: 'F#/Gb' },
-		{ value: 'G', label: 'G' },
-		{ value: 'Ab', label: 'G#/Ab' },
-		{ value: 'A', label: 'A' },
-		{ value: 'Bb', label: 'A#/Bb' },
-		{ value: 'B', label: 'B' },
-	]
-
-	const scaleTypeOptions: Option[] = [
-		{ value: 'Major', label: 'Major' },
-		{ value: 'Minor', label: 'Minor' },
-	]
 
 	const onNoteChange = (option: Option | null): void => {
 		if (option !== null) {
@@ -52,30 +28,39 @@ function App() {
 			<header>
 				<h1>Guitar Scales</h1>
 			</header>
-
-			<span>
-				<Select
-					options={noteOptions}
-					onChange={onNoteChange}
-					styles={{
-						control: baseStyles => ({
-							...baseStyles,
-							width: '10rem',
-						}),
-					}}
-				/>
-				<Select
-					options={scaleTypeOptions}
-					onChange={onScaleChange}
-					styles={{
-						control: baseStyles => ({
-							...baseStyles,
-							width: '10rem',
-						}),
-					}}
-				/>
-			</span>
-			<Scale note={note} scaleType={scale} scaleTitle={scaleTitle} />
+			<section>
+				<div className='options-container'>
+					<span className='option'>
+						<label htmlFor='note-select'>Note: </label>
+						<Select
+							id='note-select'
+							options={noteOptions}
+							onChange={onNoteChange}
+							styles={{
+								control: baseStyles => ({
+									...baseStyles,
+									width: '10rem',
+								}),
+							}}
+						/>
+					</span>
+					<span className='option'>
+						<label htmlFor='scale-select'>Scale Type: </label>
+						<Select
+							id='scale-select'
+							options={scaleTypeOptions}
+							onChange={onScaleChange}
+							styles={{
+								control: baseStyles => ({
+									...baseStyles,
+									width: '10rem',
+								}),
+							}}
+						/>
+					</span>
+				</div>
+				<Scale note={note} scaleType={scale} scaleTitle={scaleTitle} />
+			</section>
 		</>
 	)
 }
